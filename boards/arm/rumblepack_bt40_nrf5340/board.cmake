f8266a379a5f9845cddf7cf81364f42862902314
Pornthep Preechayasomboon <prnthp@uw.edu>
2024-10-06 23:37:22 -0700

    add multicore board, compiles and runs

--- boards/arm/rumblepack_bt40_nrf5340/board.cmake
+++ boards/arm/rumblepack_bt40_nrf5340/board.cmake
@@ -1,8 +1,7 @@
 # Copyright (c) 2023 Nordic Semiconductor ASA
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(jlink "--device=nrf53" "--speed=4000")
-board_runner_args(pyocd "--target=nrf5340_cpuapp" "--frequency=4000000")
+board_runner_args(jlink "--device=nrf5340_xxaa_app" "--speed=4000")
 
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
