ade9a2c3dbee63d161970d32c8fe8148569a1551
Pornthep Preechayasomboon <prnthp@uw.edu>
2024-10-06 22:48:40 -0700

    Revert "[wip] add cpunet"
    This reverts commit 939379bfc9dae09363b7d4b13f023940d5ff9964.

--- boards/arm/rumblepack_bt40_nrf5340/board.cmake
+++ boards/arm/rumblepack_bt40_nrf5340/board.cmake
@@ -1,11 +1,8 @@
 # Copyright (c) 2023 Nordic Semiconductor ASA
 # SPDX-License-Identifier: Apache-2.0
 
-if(CONFIG_BOARD_RUMBLEPACK_BT40_NRF5340_CPUAPP)
-  board_runner_args(jlink "--device=nrf5340_xxaa_app" "--speed=4000")
-elseif(CONFIG_BOARD_RUMBLEPACK_BT40_NRF5340_CPUNET)
-  board_runner_args(jlink "--device=nrf5340_xxaa_net" "--speed=4000")
-endif()
+board_runner_args(jlink "--device=nrf53" "--speed=4000")
+board_runner_args(pyocd "--target=nrf5340_cpuapp" "--frequency=4000000")
 
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
